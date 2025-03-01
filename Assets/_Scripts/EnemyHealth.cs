236101f42cae679574d2a2f9b275169fb82a4f0f
trymorez <78213565+trymorez@users.noreply.github.com>
2025-03-02 07:44:37 +0900

    testing sequence disposal

--- Assets/_Scripts/EnemyHealth.cs
+++ Assets/_Scripts/EnemyHealth.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using DG.Tweening;
+using System;
 
 public class EnemyHealth : MonoBehaviour
 {
@@ -7,12 +9,17 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] float flashingTime = 0.3f;
     Color[] originalColor;
     bool isFlashing;
+    Sequence[] flashSequence;
+    Sequence[] distortSequence;
 
     void Start()
     {
+        flashSequence = new Sequence[bodyPart.Length];
+        distortSequence = new Sequence[bodyPart.Length];
         originalColor = new Color[bodyPart.Length];
         for (int i = 0; i < bodyPart.Length; i++)
         {
+            //flashSequence[i] = DOTween.Sequence();
             originalColor[i] = bodyPart[i].color;
         }
     }
@@ -25,6 +32,11 @@ public class EnemyHealth : MonoBehaviour
             hitEffect.transform.position = other.transform.position;
             other.gameObject.SetActive(false);
 
+            if (--Health < 0)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
             if (!isFlashing)
             {
                 StartFlashing();
@@ -32,22 +44,51 @@ public class EnemyHealth : MonoBehaviour
         }
     }
 
+    void OnDestroy()
+    {
+                Debug.Log("destroied");
+        //DOTween.Kill(this.gameObject);
+            DOTween.Kill(this);
+    }
+
     void StartFlashing()
     {
         isFlashing = true;
-        for (int i = 0; i < bodyPart.Length; i++)
-        {
-            bodyPart[i].enabled = true;
-        }
-        Invoke("EndFlashing", flashingTime);
-    }
 
-    void EndFlashing()
-    {
         for (int i = 0; i < bodyPart.Length; i++)
         {
-            bodyPart[i].enabled = false;
+            var index = i;
+            var currentPart = bodyPart[index];
+            var bodyColor = currentPart.color;
+            bodyColor.a = 0;
+
+            flashSequence[index] = DOTween.Sequence(this);
+            flashSequence[index]
+                .AppendCallback(() => {
+                currentPart.enabled = true;
+            });
+
+            flashSequence[index]
+                .Append(currentPart.DOColor(originalColor[index], flashingTime))
+                .Append(currentPart.DOColor(bodyColor, flashingTime))
+                .AppendCallback(() => {
+                    currentPart.enabled = false;
+                });
+
+            flashSequence[index].OnComplete(() => {
+                if (index == bodyPart.Length - 1)
+                {
+                    isFlashing = false;
+                }
+            });
+
+            distortSequence[index] = DOTween.Sequence(this);
+            var originalSize = currentPart.transform.localScale;
+            var newSize = 1.1f;
+
+            distortSequence[index]
+                .Append(currentPart.transform.DOScale(originalSize * newSize, flashingTime))
+                .Append(currentPart.transform.DOScale(originalSize, flashingTime));
         }
-        isFlashing = false;
     }
 }
