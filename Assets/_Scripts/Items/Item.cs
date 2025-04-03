90fcf464c0e8fb672fa5d78f4ceb730a03676a27
trymorez <78213565+trymorez@users.noreply.github.com>
2025-04-03 23:24:51 +0900

    V 2.75

--- Assets/_Scripts/Items/Item.cs
+++ Assets/_Scripts/Items/Item.cs
@@ -2,14 +2,9 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    [Range(0f, 1f)]
+    [Tooltip("Chance to spawn ")]
+    public float Chance = 0.3f;
 
-    void Start()
-    {
-        
-    }
 
-    void Update()
-    {
-        
-    }
 }
