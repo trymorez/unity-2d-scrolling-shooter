7df916aeb40d9cab15d1672409e1ca09b7cddd88
trymorez <78213565+trymorez@users.noreply.github.com>
2025-04-05 22:57:39 +0900

    V 2.77

--- Assets/_Scripts/Enemy/Loot.cs
+++ Assets/_Scripts/Enemy/Loot.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Loot : MonoBehaviour
 {
-    float chanceRange = 1f;
+    [SerializeField] float totalChance = 1.0f;
+    float noLootChance;
+    [SerializeField] List<Item> lootItems = new List<Item>();
 
     void Start()
     {
--- Assets/_Scripts/Items/Item.cs
+++ Assets/_Scripts/Items/Item.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     [Range(0f, 1f)]
-    [Tooltip("Chance to spawn ")]
+    [Tooltip("Chance to spawn this item")]
     public float Chance = 0.3f;
 
 
